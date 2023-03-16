edddb8599504ce8fe44a5446b22ff03888251b8b
NivasReddyP <nivasreddy2401@gmail.com>
2023-03-16 13:36:53 +0530

    f

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
+const path = require('node:path');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
-mongoose.connect('mongodb://localhost:27017')
+const app = express()
+
+mongoose.connect('mongodb://127.0.0.1')
 .then(data => console.log('sucess'))
 .catch(err => console.log('errrrrr'))
 
+const sessionStore = new MongoStore({
+  mongoUrl:'mongodb://127.0.0.1/Users',
+  mongooseConnection:mongoose.connection,
+  collection:'session',
+})
+
+//app.set('views')
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret:'likjhsdbfgoiwg8yilkj9384yhfg;',
+  resave:false,
+  saveUninitialized:true,
+  store:sessionStore,
+  cookie:{
+    maxAge:86400000
+  }
+}))
+
+app.get('/',(req,res)=>{
+  console.log(__dirname + "/views/html")
+  res.sendFile('public/index1.html' , { root : __dirname})
+  res.sendStatus(200);
+})
 
 
 
@@ -56,8 +83,4 @@ mongoose.connect('mongodb://localhost:27017')
 
 
 
-
-
-
-
-application.listen(3000)
+app.listen(3000,()=>console.log('on 3000'))
